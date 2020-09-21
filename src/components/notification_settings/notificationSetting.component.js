d103d57ecea705390b2939c61a2f5271e007f447
Godfrey <odenigbogodfrey@yahoo.com>
2020-09-21 05:42:02 +0100

    notification settings-account verification

---
+++ src/components/notification_settings/notificationSetting.component.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { View, Switch } from "native-base";
+import { MyText } from "../../utils/Index";
+import { Styles } from "./notificationSetting.style";
+import colors from "../../colors";
+import GeneralStyles from "./../../assets/styles/GeneralStyles";
+
+export default class NotificationSettingsItem extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            active: props.initialState
+        };
+    }
+
+    render() {
+        const {textH4Style} = GeneralStyles;
+        return(
+            <View style={[Styles.parentView, this.props.style]}>
+                <MyText style={[Styles.textTitle, textH4Style]}>{this.props.title}</MyText>
+                <Switch 
+                    style={[Styles.switch]} 
+                    trackColor={colors.white} 
+                    thumbColor={colors.green} 
+                    onValueChange={(v) => {this.setState({active: v}); this.props.onValueChange(v)}}
+                    value={this.state.active}
+                />
+            </View>
+        );
+    }
+}
